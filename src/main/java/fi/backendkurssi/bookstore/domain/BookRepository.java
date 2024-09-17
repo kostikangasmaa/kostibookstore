7dfd15ba274efbf4f20fb5b5c5334ffff9515e26
kostikangasmaa <kangasmaa95@gmail.com>
2024-09-17 10:17:28 +0300

    second commit rest api

--- src/main/java/fi/backendkurssi/bookstore/domain/BookRepository.java
+++ src/main/java/fi/backendkurssi/bookstore/domain/BookRepository.java
@@ -1,8 +1,12 @@
 package fi.backendkurssi.bookstore.domain;
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
-   List<Book> findByTitle(String title);
+   List<Book> findByTitle(@Param("title")String title);
 }
+
     
--- src/main/java/fi/backendkurssi/bookstore/web/BookController.java
+++ src/main/java/fi/backendkurssi/bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package fi.backendkurssi.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import fi.backendkurssi.bookstore.domain.Book;
 import fi.backendkurssi.bookstore.domain.BookRepository;
 import fi.backendkurssi.bookstore.domain.CategoryRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BookController {
@@ -24,28 +29,39 @@ public class BookController {
         return "booklist";
     }
 
-        @RequestMapping(value = "/add")
-    public String addBook(Model model){
-    	model.addAttribute("book", new Book());
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {
+        return (List<Book>) repository.findAll();
+    }
+
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId){
+        return repository.findById(bookId);
+    }
+
+    @RequestMapping(value = "/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
         model.addAttribute("categorys", crepository.findAll());
         return "addbook";
-    }     
-    
+    }
+
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(Book book){
+    public String save(Book book) {
         repository.save(book);
         return "redirect:booklist";
-    }    
+    }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
-    	repository.deleteById(bookId);
+        repository.deleteById(bookId);
         return "redirect:../booklist";
     }
 
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
     public String editBook(@PathVariable("id") Long bookId, Model model) {
-        Book book = repository.findById(bookId).orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + bookId));
+        Book book = repository.findById(bookId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + bookId));
         model.addAttribute("book", book);
         model.addAttribute("categorys", crepository.findAll());
         return "editbook";
